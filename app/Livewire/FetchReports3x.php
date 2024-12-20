195203b0039c47f89850317026c1bcceab20f62b
kavindaNWN <kavindanwn@gmail.com>
2024-12-20 16:50:01 +0530

    cookie hanle comparison table company

--- app/Livewire/FetchReports3x.php
+++ app/Livewire/FetchReports3x.php
@@ -34,16 +34,16 @@ class FetchReports3x extends Component
         // dd($this->companies);
         if (!empty($this->companies)) {
             // check has any cookie or not
-            if(isset($_COOKIE['3x_company_abn'])) {
+            if (isset($_COOKIE['3x_company_abn'])) {
                 $this->selectedCompany = $_COOKIE['3x_company_abn'];
-            }else{
+            } else {
                 $this->selectedCompany = $this->companies[0]['abn'];
             }
 
             try {
                 $response = Http::withHeaders([
                     'Authorization' => env('API_TOKEN'),
-                ])->get(env('API_URL') . '/api/reports_3x/'. $this->selectedCompany );
+                ])->get(env('API_URL') . '/api/reports_3x/' . $this->selectedCompany);
 
                 if ($response->successful()) {
                     $this->allReports = $response->json();
@@ -66,7 +66,8 @@ class FetchReports3x extends Component
         $this->selectedStatus = $status;
     }
 
-    public function changeCompany($abn){
+    public function changeCompany($abn)
+    {
         //set cookie
         setcookie('3x_company_abn', $abn, 0, "/");
         $this->selectedCompany = $abn;
@@ -87,7 +88,7 @@ class FetchReports3x extends Component
                     $response = Http::withHeaders([
                         'Authorization' => env('API_TOKEN'),
                     ])->get(env('API_URL') . '/api/companies/3x');
-        
+
                     if ($response->successful()) {
                         // dd($response->json());
                         $this->companies = $response->json();
@@ -97,21 +98,25 @@ class FetchReports3x extends Component
                 } catch (Exception $e) {
                     abort(500, 'Something went wrong');
                 }
-                
+
                 //i want to check companies has selected company
-                if(!in_array($this->selectedCompany, array_column($this->companies, 'abn'))){
-                    if(count($this->companies) > 0){
+                if (!in_array($this->selectedCompany, array_column($this->companies, 'abn'))) {
+                    if (count($this->companies) > 0) {
                         $this->selectedCompany = $this->companies[0]['abn'];
                     }
                     setcookie('3x_company_abn', '', time() - 3600, "/");
                     // setcookie('3x_company_abn', $this->selectedCompany, 0, "/");
                 }
-                // if (isset($_SESSION['3x_company_comparison_abn'])) {
-                //     if (!in_array($_SESSION['3x_company_comparison_abn'], array_column($this->companies, 'abn'))) {
-                //         setcookie('3x_company_comparison_abn', '', time() - 3600, "/");
-                //     }
-                // }
+                if (!empty($_COOKIE['3x_company_comparison_abn']) && count($this->companies) > 0) {
+                    $cookieValue = $_COOKIE['3x_company_comparison_abn'];
+                    $abnList = array_column($this->companies, 'abn');
                 
+                    if (!in_array($cookieValue, $abnList)) {
+                        setcookie('3x_company_comparison_abn', '', time() - 3600, "/");
+                    }
+                }
+                
+
 
                 $this->loadData();
             } else {
@@ -131,8 +136,8 @@ class FetchReports3x extends Component
             try {
                 $response = Http::withHeaders([
                     'Authorization' => env('API_TOKEN'),
-                ])->get(env('API_URL') . '/api/reports_3x/'. $this->selectedCompany);
-    
+                ])->get(env('API_URL') . '/api/reports_3x/' . $this->selectedCompany);
+
                 if ($response->successful()) {
                     $this->allReports = $response->json();
                 } else {
